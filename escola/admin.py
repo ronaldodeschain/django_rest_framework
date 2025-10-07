ba98688f77c5bd32b8cd130729bc431c6df4e44f
RONALDO LEMOS DA SILVA <01300771003@senacrs.edu.br>
2025-10-06 21:41:31 -0300

    terminando o estudo por hoje

--- escola/admin.py
+++ escola/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from escola.models import Estudante,Curso
+from escola.models import Estudante,Curso,Matricula
 
 class Estudantes(admin.ModelAdmin):
     list_display = ('id','nome','email','cpf','data_nascimento','celular')
@@ -16,3 +16,8 @@ class cursos(admin.ModelAdmin):
 
 admin.site.register(Curso,cursos)
 
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id','estudante','curso','periodo')
+    list_display_links = ('id',)
+
+admin.site.register(Matricula,Matriculas)
